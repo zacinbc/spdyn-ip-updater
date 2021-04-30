43ae662e2096fce7c9825c47b4eee2d3290a01dd
numselli <58607232+numselli@users.noreply.github.com>
2021-04-30 15:23:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const IpMonitor = require('ip-monitor'),querystring = require("querystring"), https = require('https');
+const IpMonitor = require('ip-monitor'), axios = require('axios');
 
-const hostname = "",token = "";
+const hostname = "", token = "";
 
 const ipMonitor = new IpMonitor({pollingInterval: 36000,verbose: true,externalIp: {timeout: 1000,getIP: 'parallel',services: ['http://icanhazip.com/'],replace: true,verbose: true}});
-ipMonitor.on('error', (error) => {
-    console.error(error);
-});
+ipMonitor.on('error', (error) => console.error(error));
+
 ipMonitor.start();
 ipMonitor.on('change', (prevIp, newIp) => {
-    if(prevIp != null && newIp != null && prevIp!=newIp){
-        var result = `https://update.spdyn.de/nic/update?${querystring.stringify({'hostname': hostname, 'myip': newIp, 'user': hostname, 'pass': token})}`;
-        https.get(result, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                console.log(data);
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-        console.log(`IP changed from ${prevIp} to ${newIp}`);
+    if(prevIp && newIp && prevIp!=newIp){
+        axios.get(`https://update.spdyn.de/nic/update?hostname=${hostname}&myip=${newIp}&user=hostname&pass=token`)
+        .then((response)=> {
+            if (response.ok) console.log('DDNS IP successfully updated');
+        })
+        .catch((error)=>console.loerror))
     }
 });
