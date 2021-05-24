2ecc347ef9ee3fbb8635659727d8cedf6e04f3a2
numselli <58607232+numselli@users.noreply.github.com>
2021-05-23 17:16:18 -0700

    add error catching

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let axios = require('axios'),lastIP , hostname = "", token = "";
 
 async function checkIP() { 
     let ipRequest = await axios.get('https://api.my-ip.io/ip.json');
-    if (ipRequest.status === 200){
-        console.log("got valid IP")
-        if (lastIP !== ipRequest.data.ip){
-            console.log(`new IP: ${ipRequest.data.ip}`)
-            let updateResponce = await axios.get(`https://www.duckdns.org/update?domains=${hostname}&token=${token}&verbose=true`)
-            console.log(updateResponce.data)       
-        }
-    }else console.error("there was an error getting new IP")
+    if (!ipRequest.status === 200) return console.error("there was an error getting new IP")
+
+    console.log("got valid IP")
+    if (lastIP === ipRequest.data.ip) return
+    
+    console.log(`new IP: ${ipRequest.data.ip}`)
+    let updateResponce = await axios.get(`https://www.duckdns.org/update?domains=${hostname}&token=${token}&verbose=true`).catch((error) => console.log('Error', error.message));
+    console.log(updateResponce.data)       
 }
 checkIP()
 setInterval(checkIP, 7200000);
